ccbff2e3b5af113d553005414db3d58482b7af06
yakalkode <yakalkode@yahoo.com>
2014-02-08 20:03:25 +0100

    Remove hash in video file name.

--- dailymotion_fetcher.sh
+++ dailymotion_fetcher.sh
@@ -6,8 +6,8 @@ if [ $# -ne 1 ]; then
 fi
 
 URL=$1
-BASENAME=`basename $URL | cut -d'#' -f1 | cut -d'?' -f1`
-FILENAME=${BASENAME}.mp4
+BASENAME=`basename $URL | cut -d# -f1 | cut -d? -f1`
+FILENAME=`echo ${BASENAME} | cut -d_ -f2`.mp4
 JSON_URL=http://www.dailymotion.com/json/video/${BASENAME}?fields=stream_h264_url,stream_h264_ld_url,stream_h264_hq_url,stream_h264_hd_url,stream_h264_hd1080_url
 
 wget -O - -q $JSON_URL | tr ',' '\n' | sed 's/^.*\(http:.*\)"/\1/' | grep http | tail -n 1 | xargs wget -O $FILENAME
