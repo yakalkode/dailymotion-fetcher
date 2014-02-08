e8090ac053f37b79352859b1c508c167f250336d
yakalkode <yakalkode@yahoo.com>
2014-02-08 19:46:01 +0100

    Add dailymotion_fetcher.sh

---
+++ dailymotion_fetcher.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <DAILYMOTION_URL>"
+    exit 1
+fi
+
+URL=$1
+BASENAME=`basename $URL | cut -d'#' -f1`
+FILENAME=${BASENAME}.mp4
+JSON_URL=http://www.dailymotion.com/json/video/${BASENAME}?fields=stream_h264_url,stream_h264_ld_url,stream_h264_hq_url,stream_h264_hd_url,stream_h264_hd1080_url
+
+wget -O - -q $JSON_URL | tr ',' '\n' | sed 's/^.*\(http:.*\)"/\1/' | grep http | tail -n 1 | xargs wget -O $FILENAME
+
+
+
